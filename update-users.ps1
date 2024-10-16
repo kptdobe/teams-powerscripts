034baf86e403ec3392714366bf22604daa6ad2f6
kptdobe <acapt@adobe.com>
2024-10-16 14:41:47 +0200

    chore: only do op once...

--- update-users.ps1
+++ update-users.ps1
@@ -1,15 +1,15 @@
 
 
-# $photo = (Get-Item .).FullName + "/adobe-logo.png"
-$photo = (Get-Item .).FullName + "/helix-logo.png"
+$photo = (Get-Item .).FullName + "/adobe-logo.png"
+# $photo = (Get-Item .).FullName + "/helix-logo.png"
 $company = "Adobe"
 
 Connect-MgGraph -Scopes 'User.ReadWrite.All' -NoWelcome
 Connect-Entra -Scopes 'User.ReadWrite.All' -NoWelcome
 
-# $users = Get-MgUser -Filter "endsWith(mail,'adobe.com')" -ConsistencyLevel eventual -CountVariable countVar
+$users = Get-MgUser -Filter "endsWith(mail,'adobe.com')" -ConsistencyLevel eventual -CountVariable countVar
 # $users = Get-MgUser -Filter "endsWith(mail,'@adobe.com') and NOT(CompanyName eq 'Adobe')" -ConsistencyLevel eventual -CountVariable countVar
-$users = Get-MgUser -Filter "endsWith(mail,'@AdobeEnterpriseSupportAEM.onmicrosoft.com') and startsWith(DisplayName,'admin')" -ConsistencyLevel eventual -CountVariable countVar
+# $users = Get-MgUser -Filter "endsWith(mail,'@AdobeEnterpriseSupportAEM.onmicrosoft.com') and startsWith(DisplayName,'admin')" -ConsistencyLevel eventual -CountVariable countVar
 
 foreach($user in $users){
     try {
@@ -18,19 +18,15 @@ foreach($user in $users){
       $mail = $user.Mail
       $id = $user.Id
 
-      Set-MgUserPhotoContent -UserId $id -InFile $photo
-      Write-Host "Updating in Entra: $username's photo has been updated!"
+      Set-EntraUserThumbnailPhoto -UserId $id -FilePath $photo
+      if ($res) {
+        Write-Host "Updating in Entra: $username's photo has been updated!"
+      } else {
+        exit;
+      }
 
       Update-MgUser -UserId $id -CompanyName $company
       Write-Host "Updating in Entra: $username's company has been updated!"
-
-      $teamsUser = Get-EntraUser -Filter "Mail eq '$mail'"
-
-      Set-EntraUserThumbnailPhoto -ObjectId $teamsUser.id -FilePath $photo
-      Write-Host "Updating in Teams: $username's photo has been updated!"
-
-      Set-EntraUser -ObjectId $teamsUser.id -CompanyName $company
-      Write-Host "Updating in Teams: $username's company has been updated!"
     }
     catch {
        Write-Host $Error
